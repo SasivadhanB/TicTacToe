110e331bd2876bf476290af2f0c7956dc43e26b6
SasivadhanB <sasirock341@gmail.com>
2026-04-21 15:44:29 +0530

    Create UC1.java

---
+++ UC1.java
@@ -0,0 +1,24 @@
+public class TicTacToe {
+    public static void main(String[] args) {
+
+        char[][] board = new char[3][3];
+
+        // Fill board with '-'
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+        }
+
+        // Print board
+        System.out.println("-------------");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " | ");
+            }
+            System.out.println();
+            System.out.println("-------------");
+        }
+    }
+}
